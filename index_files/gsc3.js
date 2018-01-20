d4f30db2a8d9d5808126491acd528b89fccc1221
JasonTheIncredible <35417691+JasonTheIncredible@users.noreply.github.com>
2018-01-19 22:14:46 -0800

    Changed the search box for 2 million pixels

--- index_files/gsc3.js
+++ index_files/gsc3.js
@@ -73,8 +73,8 @@ function mtrack(e) {
 	if (navigator.appName == 'Netscape') {
 		document.getElementById('d').style.left=xcurs + 10;
 		document.getElementById('d').style.top=ycurs;
-		l_xcurs = Math.ceil((xcurs - lhs)*1.262) + 12; //was  (xcurs - lhs)/10)+1(?) KEEP MANIPULATING THIS//
-		l_ycurs = Math.ceil((ycurs - headerHeight)*1.137)-3; //was  ((ycurs - headerHeight)/10)//
+		l_xcurs = Math.ceil((xcurs - lhs)*.6575) + 6; //was  (xcurs - lhs)/10)+1(?), for 3 mill: ((xcurs - lhs)*1.262) + 12//
+		l_ycurs = Math.ceil((ycurs - headerHeight)*.7266)-2; //was  ((ycurs - headerHeight)/10), for 3 mill: ((ycurs - headerHeight)*1.137)-3//
 	} else {
 		l_xcurs = Math.ceil((xcurs - lhs)/10) + 1;
 		l_ycurs = Math.ceil((ycurs - headerHeight + document.body.scrollTop)/10);
@@ -84,7 +84,7 @@ function mtrack(e) {
 
 
 
-	if ((l_ycurs <= 0) || (l_ycurs > 1250) || (l_xcurs <= 0) || (l_xcurs > 2400)){ //Used for hover info panel on empty pixels
+	if ((l_ycurs <= 0) || (l_ycurs > 800) || (l_xcurs <= 0) || (l_xcurs > 1250)){ //Used for hover info panel on empty pixels, for 3 million: ((l_ycurs <= 0) || (l_ycurs > 1250) || (l_xcurs <= 0) || (l_xcurs > 2400))//
 		document.getElementById('d').style.display = "none";
 	} else {
 		document.getElementById('d').style.display = "";
