306fdfaf897dadde80d9d5e4b54d2baa6628873e
JasonTheIncredible <35417691+JasonTheIncredible@users.noreply.github.com>
2018-02-18 16:36:49 -0800

    Current failed attempt at making "true" random

--- index_files/randomRequest.js
+++ index_files/randomRequest.js
@@ -1,30 +1,27 @@
 function getNewThing(apiUrl) {
 
-    data = {
-        "jsonrpc": "2.0",
-        "method": "generateIntegers",
-        "params": {
-            "apiKey": "96f04c28-ee5d-446e-a763-e1c1f3c867a9",
-            "n": 1,
-            "min": 1,
-            "max": 1000,
-            "replacement": true,
-            "base": 10
-        },
-        "id": 884832
-    }
+   var data: {
+  'jsonrpc': '2.0',
+  'method': 'generateIntegers',
+  'params': {
+    'apiKey': '96f04c28-ee5d-446e-a763-e1c1f3c867a9',
+    'n': 1,
+    'min': 1,
+    'max': 1000,
+    'replacement': true,
+    'base': 10
+  },
+  'id': 2601
+};
 
-    // ajax call to the api
-    return $.ajax({
-        type: "POST",
-        url: "https://api.random.org/json-rpc/1/invoke",
-        data: JSON.stringify(data),
-        success: function(result) {
-        	$('#result').html(data);
-            console.log(result)
-        },
-        error: function(err) {
-            console.log(err)
-        }
-    });
-}
+$.ajax({
+  url: 'https://api.random.org/json-rpc/1/invoke',
+  type: "POST",
+  data: JSON.stringify(data),// stringify data object
+  contentType: "application/json; charset=utf-8",
+  dataType: "json",
+  success: function(result) {
+    $('#text').html(JSON.stringify(result));
+    console.log(result);
+  }
+})};
