65bf6d8196a0bb9e03f4db701d3c1a4183a6cc87
JasonTheIncredible <35417691+JasonTheIncredible@users.noreply.github.com>
2018-03-15 21:37:43 -0700

    Got rid of YouTube controls

--- index_files/modal.js
+++ index_files/modal.js
@@ -39,7 +39,7 @@ console.log($videoSrc);
 $('#myModal').on('shown.bs.modal', function (e) {
     
 // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-$("#video").attr('src',$videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1" ); 
+$("#video").attr('src',$videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1&controls=0" ); 
 })
   
   
