4f2d79ddf81b5072464ba032f6ed9241071b0d7c
JasonTheIncredible <35417691+JasonTheIncredible@users.noreply.github.com>
2018-03-21 23:15:39 -0700

    Made AMOE button work. Now I need to learn how to edit the modal content.

--- index_files/modal.js
+++ index_files/modal.js
@@ -37,25 +37,35 @@ console.log($videoSrc);
 
 // when the modal is opened autoplay it  
 $('#myModal').on('shown.bs.modal', function (e) {
-    
+if ($('.moreinfo').length){
+
+}
+else{
 // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
 $("#video").attr('src',$videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1&controls=0" ); 
-})
+}
+});
   
   
 // stop playing the youtube video when I close the modal
 $('#myModal').on('hide.bs.modal', function (e) {
+if ($('.moreinfo').length){
+	location.reload();
+}
+else{
     // a poor man's stop video
     $("#video").attr('src',$videoSrc);
     $("#maybewinner").prop("disabled", true);
     $carry = false;
     location.reload();
+	}
 })
 
 
 // Have random compare "winner" button appear after time//
-	$( ".video-btn" ).click(function() {
-	$("#maybewinner").prop("disabled", true);
+$(".video-btn").click(function() {
+	
+		$("#maybewinner").prop("disabled", true);
        	
        	setTimeout(function(){
         	$("#maybewinner").text("Discover if you've won in 7")}, 1000);
@@ -81,7 +91,6 @@ $('#myModal').on('hide.bs.modal', function (e) {
         setTimeout(function(){
         	$("#maybewinner").prop("disabled", false); $("#maybewinner").text("Click here to discover if you've won")}, 8000);
 		});
-
 $('#amoeModal').on('shown.bs.modal', function (e) {
     
 $("p").text("Hello world!");
