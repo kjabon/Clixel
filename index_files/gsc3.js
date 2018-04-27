b1bfd3174d7ca3d0f3a94e13d0e25f1d630fce6d
JasonTheIncredible <35417691+JasonTheIncredible@users.noreply.github.com>
2018-04-26 22:59:08 -0700

    Modified main js file

--- index_files/gsc3.js
+++ index_files/gsc3.js
@@ -1,54 +1,7 @@
-
-
-//Basically I don't really get yet where it draws the box on the page, and I'd like to change it so that when your mouse is on the right, the box gets drawn to the left of the mouse.
-
-
-/*
-     FILE ARCHIVED ON 5:47:20 Aug 6, 2015 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 20:45:17 Aug 24, 2015.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
 var total = 1731.87;
 var totalwithcommas = total.toLocaleString();
 
-var bH = false;
-var bZ = false;
 var showMode = false;
-function tn() {
-	da();
-	document.getElementById('neg').style.display="block";
-	document.getElementById('sn').src = "img-pix/btnneg1.gif";
-	showMode = true;
-}
-function th() {
-	da();
-	document.getElementById('f').style.display="block";
-	bH = true;
-}
-function tz() {
-	da();
-	document.getElementById('sz').src = "img-pix/btnzoom1.gif";
-	bZ = true;
-	document.getElementById('een').style.display="block";
-	showMode = true;
-}
-function da2() {
-	da();
-	document.getElementById('so').src = "img-pix/btnnor1.gif";	
-	showMode = false;
-}
-function da() {
-	document.getElementById('f').style.display="none";
-	document.getElementById('een').style.display="none";
-	document.getElementById('neg').style.display="none";
-	document.getElementById('sn').src = "img-pix/btnneg0.gif";
-	document.getElementById('sz').src = "img-pix/btnzoom0.gif";
-	document.getElementById('so').src = "img-pix/btnnor0.gif";
-	bH = false;
-}
 
 function amoeContinue(){
 	document.getElementById("amoebtncontinue").style.display="none";
@@ -68,6 +21,7 @@ var l_ycurs;
 var l_xcurs_random=Math.floor(Math.random() * 1000) + 1;
 var l_ycurs_random=Math.floor(Math.random() * 1000) + 1;
 
+ //compares the randomly selected number to the one the participant chose//
 function randomCompare(){
 	
 		if (l_xcurs > 1000){
@@ -90,7 +44,7 @@ function randomCompare(){
 			alert("I'm sorry, but you chose (" + l_xcurs + "," + l_ycurs + ") and the winning pixel was (" + l_xcurs_random + "," + l_ycurs_random + ").\nYou were " + Math.round(Math.sqrt((Math.pow(l_xcurs - l_xcurs_random, 2) + Math.pow(l_ycurs - l_ycurs_random, 2)))) + " pixels away from the winning pixel.");
 			location.reload();
 		}
-} //compares the randomly selected number to the one the participant chose//
+}
 
 function winnerexample(){
 
@@ -106,7 +60,6 @@ function winnerexample(){
 		}
 		else{
 		}
-
 }
 
 // turns total into 10% and adds appropriate commas //
@@ -262,43 +215,10 @@ if (navigator.appName == 'Netscape') {
 
 }
 
-	if (!bZ) {
-		return;
-	}
-	var x,y,x1,x2,y1,y2,dx=0,dy=0;
-	fact=2;
-	opp=100;
-
-	x=event.x;
-	y=event.y;
-	   
-	dx=window.document.body.scrollLeft;
-	dy=window.document.body.scrollTop;
-
-	x1=-opp+(x+dx)*fact;	//left
-	y1=-opp+(y+dy)*fact - (document.body.scrollTop*fact);	//top
-	x2=+opp+(x+dx)*fact;	//right
-	y2=+opp+(y+dy)*fact - (document.body.scrollTop*fact);	//bottom
-
-	document.getElementById('een').style.left=(x+dx)*(1-fact);
-	document.getElementById('een').style.top=(y+dy)*(1-fact) + document.body.scrollTop;
-	document.getElementById('een').style.clip="rect(" +y1 +"px," +x2 +"px," +y2 +"px,"+x1 +"px)";
-
-}
-
-function h(iL, iT, iW, iH) {
-	if (!bH) {return;}
-	document.getElementById('f').style.left=iL - 1;
-	document.getElementById('f').style.top=64+iT;
-	document.getElementById('f').style.width=iW + 4;
-	document.getElementById('f').style.height=iH + 4;
 }
 
 var sTitle="";
 function d(o) {
 
 	sTitle = o.title;
-}
-function e(o) {
-	sTitle = "";	
 }
